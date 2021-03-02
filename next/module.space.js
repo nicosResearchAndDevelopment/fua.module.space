bcf71517d985cfeedbf2bbab392ed3899c739437
Simon Petrac <pfoerdie@gmail.com>
2021-03-02 14:23:09 +0100

    wip: works, but I am not happy with this solution because it is not future-proof in my opinion

--- next/module.space.js
+++ next/module.space.js
@@ -1,80 +1,105 @@
 const
-    _             = require('./util.js'),
-    {
-        TermFactory, DataFactory, Dataset, DataStore
-    }             = require('@nrd/fua.module.persistence'),
-    InmemoryStore = require('@nrd/fua.module.persistence.inmemory'),
-    loadSpace     = require('./module.space.load.js');
+    _                                 = require('./util.js'),
+    loadSpace                         = require('./module.space.load.js'),
+    {DataFactory, Dataset, DataStore} = require('@nrd/fua.module.persistence'),
+    InmemoryStore                     = require('@nrd/fua.module.persistence.inmemory');
+
+class SpaceNode {
+
+    constructor(space, id) {
+        this.space  = space;
+        this['@id'] = id;
+        _.lockProp(this, 'space', '@id');
+    } // SpaceNode#constructor
+
+    async load() {
+        _.assert(!this['@type'], 'SpaceNode#load : already loaded');
+
+        const
+            factory  = this.space.factory,
+            store    = this.space.store,
+            subject  = this['@id'].startsWith('_:')
+                ? factory.blankNode(this['@id'].substr(2))
+                : factory.namedNode(this['@id']),
+            /** @type {Dataset} */
+            subjData = await store.match(subject),
+            rdf_type = factory.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type').value;
+
+        this['@type'] = [];
+        _.lockProp(this, '@type');
+
+        for (let {predicate, object} of subjData) {
+            const
+                key  = predicate.value,
+                prop = this[key] || (this[key] = []);
+
+            if (key === rdf_type) {
+                this['@type'].push(object.value);
+            } else {
+                switch (object.termType) {
+                    case 'NamedNode':
+                        prop.push(new SpaceNode(this.space, object.value));
+                        break;
+                    case 'BlankNode':
+                        prop.push(new SpaceNode(this.space, '_:' + object.value));
+                        break;
+                    case 'Literal':
+                        prop.push(object.language
+                            ? {'@value': object.value, '@language': object.language}
+                            : {'@value': object.value, '@type': object.datatype});
+                        break;
+                } // switch
+            }
+        } // for
+
+        return this;
+    } // SpaceNode#load
+
+} // SpaceNode
 
 class Space {
 
-    /** @type {Dataset} */
-    #dataset   = null;
-    /** @type {DataStore} */
-    #dataStore = null;
-
     constructor(options) {
         _.assert(_.isObject(options), 'Space#constructor : invalid options', TypeError);
-        _.assert(!_.isDefined(options.dataset) || (options.dataset instanceof Dataset),
+        if (_.isDefined(options.dataset)) _.assert(options.dataset instanceof Dataset,
             'Space#constructor : invalid options.dataset', TypeError);
-        _.assert(!_.isDefined(options.dataStore) || (options.dataStore instanceof DataStore),
+        if (_.isDefined(options.dataStore)) _.assert(options.dataStore instanceof DataStore,
             'Space#constructor : invalid options.dataStore', TypeError);
-        _.assert(!_.isDefined(options.context) || _.isObject(options.context),
+        if (_.isDefined(options.factory)) _.assert(options.factory instanceof DataFactory,
+            'Space#constructor : invalid options.factory', TypeError);
+        if (_.isDefined(options.context)) _.assert(_.isObject(options.context),
             'Space#constructor : invalid options.context', TypeError);
 
-        this.termFactory = new TermFactory(options.context);
-        this.dataFactory = new DataFactory(options.context);
-        _.lockProp(this, 'termFactory', 'dataFactory');
+        const
+            factory   = options.factory || (options.dataset ? options.dataset.factory : options.dataStore ? options.dataStore.factory : new DataFactory(options.context)),
+            dataset   = options.dataset || new Dataset(null, factory),
+            dataStore = options.dataStore || new InmemoryStore(null, factory);
 
-        this.#dataset   = options.dataset || new Dataset(null, this.dataFactory);
-        this.#dataStore = options.dataStore || new InmemoryStore(null, this.dataFactory);
+        _.assert(factory === dataset.factory && factory === dataStore.factory,
+            'Space#cosntructor : different factories are not allowed');
+
+        this.data    = dataset;
+        this.store   = dataStore;
+        this.factory = factory;
+        _.lockProp(this, 'dataset', 'dataStore', 'factory');
     } // Space#constructor
 
     async load(param) {
         const resultArr = await loadSpace(param);
         for (let result of resultArr) {
             if (result.dataset)
-                this.#dataset.add(result.dataset);
+                this.data.add(result.dataset);
         }
     } // Space#load
 
     async getNode(subjectIRI) {
-        _.assert(_.isString(subjectIRI), 'Space#getNode : invalid subjectIRI', TypeError);
+        if (this.factory.isNamedNode(subjectIRI)) subjectIRI = subjectIRI.value;
+        else if (this.factory.isBlankNode(subjectIRI)) subjectIRI = '_:' + subjectIRI.value;
+        else _.assert(_.isString(subjectIRI), 'Space#getNode : invalid subjectIRI', TypeError);
 
-        const
-            subject  = subjectIRI.startsWith('_:')
-                ? this.dataFactory.blankNode(subjectIRI.substr(2))
-                : this.dataFactory.namedNode(subjectIRI),
-            /** @type {Dataset} */
-            subjData = await this.#dataStore.match(subject);
+        const subjNode = new SpaceNode(this, subjectIRI);
+        await subjNode.load();
 
-        if (subjData.size === 0) return null;
-        const subjNode = {'@id': subject.value};
-
-        for (let {predicate, object} of subjData) {
-            const
-                key  = predicate.value,
-                prop = subjNode[key] || (subjNode[key] = []);
-            switch (object.termType) {
-                case 'NamedNode':
-                    prop.push({'@id': object.value});
-                    break;
-                case 'BlankNode':
-                    prop.push({'@id': '_:' + object.value});
-                    break;
-                case 'Literal':
-                    prop.push(object.language
-                        ? {'@value': object.value, '@language': object.language}
-                        : {'@value': object.value, '@type': object.datatype});
-                    break;
-            }
-        }
-
-        const rdf_type = this.dataFactory.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type').value;
-        if (subjNode[rdf_type]) {
-            subjNode['@type'] = subjNode[rdf_type].map(node => node['@id']);
-            delete subjNode[rdf_type];
-        }
         return subjNode;
     } // Space#getNode
 
