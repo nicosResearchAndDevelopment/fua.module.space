23d74db6fe5cff8b1d9f68fa8501b87250e74f96
Simon Petrac <pfoerdie@gmail.com>
2021-11-10 16:38:19 +0100

    changed return value of Node#load and Node#save

--- src/module.space.Node.js
+++ src/module.space.Node.js
@@ -340,7 +340,7 @@ module.exports = class Node extends _.ProtectedEmitter {
 
     /**
      * @param {Array<string>} [props]
-     * @returns {Promise<boolean>}
+     * @returns {Promise<this>}
      */
     async load(props) {
         const store = this.#space.getStore(_.SECRET);
@@ -386,12 +386,12 @@ module.exports = class Node extends _.ProtectedEmitter {
             this.#allPropsLoaded = true;
         }
         this.#space.emit(_.SECRET, _.events.node_loaded, this);
-        return this.#loadedData.size > 0;
+        return this;
     } // Node#load
 
     /**
      * @param {Array<string>} [props]
-     * @returns {Promise<boolean>}
+     * @returns {Promise<this>}
      */
     async save(props) {
         const store = this.#space.getStore(_.SECRET);
@@ -420,7 +420,7 @@ module.exports = class Node extends _.ProtectedEmitter {
         if (addCount) this.#loadedData.add(addData);
         if (deleteCount) this.#loadedData.delete(deleteData);
         this.#space.emit(_.SECRET, _.events.node_saved, this);
-        return addCount + deleteCount > 0;
+        return this;
     } // Node#save
 
     async delete() {
